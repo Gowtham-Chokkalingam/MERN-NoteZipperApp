46875d6a0e30e368a6506e8bf5a5832d26b86fe8
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-11-27 21:03:55 +0530

    Front end UI added and Routers are done

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
+// >Importing express for server
 const express = require("express");
 const app = express();
+
+// >Importing dotenv for safegaurding the port server
+const dotenv = require("dotenv");
+dotenv.config();
+
+const notes = require("./data/notes");
 
 app.get("/", (req, res) => {
   res.send("API is Running");
 });
 
-app.listen("8080", (req, res) => {
-  console.log("Server Runs in Port 8080");
+app.get("/api/notes", (req, res) => {
+  res.send(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const singleNote = notes.find((n) => n._id === req.params.id);
+  res.send(singleNote);
+});
+
+const PORT = process.env.PORT || 5000;
+console.log("PORT:", process.env.PORT);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Runs in Port ${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import LandingPage from './screens/LandingPage';
+import MyNotes from './screens/MyNotes/MyNotes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header></Header>
+    <main>
+     <Routes>
+     <Route path="/" element={<LandingPage/>} />
+     <Route path="/mynotes" element={<MyNotes/>} />
+     </Routes>
+    </main>
+    <Footer></Footer>
     </div>
   );
 }
